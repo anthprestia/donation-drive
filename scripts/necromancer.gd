0af9b4c9af61954c9270c6430b9da7e6771ffd17
anthprestia <anthprestia@gmail.com>
2026-06-08 13:15:03 -0400

    Initial commit

---
+++ scripts/necromancer.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $NecroAnimation
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _ready() -> void:
+	_animated_sprite.play("idle")
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	
+	velocity.x = direction_x * SPEED
+	velocity.y = direction_y * SPEED
+
+
+	move_and_slide()
+
+
+func _on_body_entered(body: Node) -> void:
+	print("hi")
+	pass # Replace with function body.
